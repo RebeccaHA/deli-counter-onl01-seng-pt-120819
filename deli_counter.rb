4d16654b738ada6828562aa5a2f156c7f41b6729
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-13 18:40:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli= []
 
 def line(array)
   new_array = []
-  if new_array == 0 
+  if new_array.length == 0 
     puts "The line is currently empty."
   else
     array.each_with_index do |name, index|
